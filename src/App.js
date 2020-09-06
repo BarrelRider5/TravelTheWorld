4309ec5bb3a4846f68d7fe0488b73ec3933b2c5a
Jordan Mulder <utahsamurai@gmail.com>
2020-09-05 22:05:53 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom'
 
-import Classes from './Classes'
-import Footer from './components/Footer'
 import Header from './components/Header'
-import Home from './Home'
-import Instructor from './Instructor'
-import Register from './Register'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
-export default () => <div className="App">
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/classes" component={Classes} />
-      <Route path="/instructor" component={Instructor} />
-      <Route path="/register" component={Register} />
-      <Route path="/" component={Home} />
-    </Switch>
-    <Footer />
-  </Router>
-</div>
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/profile" component={Profile} />
+            <Route path="/*" component={Home} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/pages/Profile.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+    <div>This is the profile page</div>
+)
